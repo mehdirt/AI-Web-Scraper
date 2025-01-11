251fe01c4564fc5f0480b9e9dd61eaf9bbf871b7
mehdirt <mahdirafati680@gmail.com>
2025-01-11 14:27:21 +0330

    Feat: Captcha handling using brightdata

--- scrape.py
+++ scrape.py
@@ -1,22 +1,22 @@
-import selenium.webdriver as webdriver
-from selenium.webdriver.chrome.service import Service
-import time
-
+from selenium.webdriver import Remote, ChromeOptions
+from selenium.webdriver.chromium.remote_connection import ChromiumRemoteConnection
+from selenium.webdriver.common.by import By
+ 
+AUTH = 'brd-customer-hl_29454674-zone-ai_scraper:jtrzxe76kv15'
+SBR_WEBDRIVER = f'https://{AUTH}@brd.superproxy.io:9515'
 def scrape_website(website):
     """"""
     print("Lunching chrome browser...")
 
-    chrome_driver_path = "./chromedriver"
-    options = webdriver.ChromeOptions()
-    driver = webdriver.Chrome(service=Service(chrome_driver_path), options=options)
-
-    try:
+    print('Connecting to Scraping Browser...')
+    sbr_connection = ChromiumRemoteConnection(SBR_WEBDRIVER, 'goog', 'chrome')
+    with Remote(sbr_connection, options=ChromeOptions()) as driver:
+        print('Connected! Navigating...')
         driver.get(website)
-        print("Page loaded...")
+        print('Taking page screenshot to file page.png')
+        driver.get_screenshot_as_file('./page.png')
+        print('Navigated! Scraping page content...')
         html = driver.page_source
-        time.sleep(10)
-
         return html
-        
-    finally:
-        driver.quit()
+
+
